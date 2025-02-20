dfd6b27d23ab824ca57e942efe45244e9f8b463c
Your GitHub Username <sagarbadal70@gmail.com>
2025-02-20 16:45:46 +0530

    crypto-hashing in name and email funciton added

---
+++ helper/crypto.js
@@ -0,0 +1,25 @@
+import crypto from 'crypto';
+import dotenv from 'dotenv';
+dotenv.config();
+const algorithm='aes-256-cbc';
+const key=Buffer.from(process.env.ENCRYPTION_KEY, 'hex');//256 bit or 64 byte
+const iv=crypto.randomBytes(16);//16 bytes of 64 bits
+// console.log(key.toString('hex'))
+// console.log(iv)
+export const encrypt=(text)=>{
+  const cipher=crypto.createCipheriv(algorithm,key,iv);
+  let encrypted=cipher.update(text,'utf8','hex');//hexa partial-conversion
+//   console.log(encrypted)
+  encrypted+=cipher.final('hex');
+  return `${iv.toString('hex')}:${encrypted}`;
+}
+
+export const decrypt=(encryptedText)=> {
+  const [ivHex,data] = encryptedText.split(':');
+  const decipher=crypto.createDecipheriv(algorithm,key,Buffer.from(ivHex, 'hex'));
+    let decrypted=decipher.update(data,'hex','utf8');
+    //   console.log(decrypted)
+  decrypted+=decipher.final('utf8');
+  return decrypted;
+}
+
